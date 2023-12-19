2b73b0c2de3b35a0055356b7d9800ee424afa09e
James Drury <JDrury@mathematica-mpr.com>
2023-12-19 16:32:12 -0700

    created functions

--- player_shooting_for_individual_game.R
+++ player_shooting_for_individual_game.R
@@ -1,7 +1,4 @@
 # get player shooting charts for a specific game
-# updated the comment
-library(tidyverse)
-# here we go
 
 season <- 2022
 
@@ -12,71 +9,79 @@ d <- games_player_team
 
 ### all shooting stats
 
-d %>%
-  filter(dateGame == "2021-10-30" &
-         (slugTeam == "UTA" |
-         slugOpponent == "UTA")) %>%
-  group_by(slugTeam) %>%
-  top_n(n = 3, wt = pts) %>%
-  ungroup() %>%
-  arrange(slugTeam) %>%
-  pivot_longer(cols = c("fga", "fgm", "fta", "ftm", "fg2a", "fg2m", "fg3m", "fg3a"), 
-               names_to = "made_miss",
-               values_to = "value") %>%
-  mutate(shot_type = case_when(
-    made_miss %in% c("fga", "fgm") ~ "All FGs",
-    made_miss %in% c("fg2a", "fg2m") ~ "2-pt FGs",
-    made_miss %in% c("fg3a", "fg3m") ~ "3-pt FGs",
-    made_miss %in% c("fta", "ftm") ~ "FTs"
-  )) %>%
-  ggplot(aes(x = shot_type, y = value)) +
-  geom_bar(data = . %>% filter(made_miss %in% c("fg3a", "fg2a", "fta")),
-           aes(fill = "Shots taken"),
-           color = "darkolivegreen3",
-           alpha = 0.5,
-           stat = "identity",
-           position = "dodge") +
-  geom_bar(data = . %>% filter(made_miss %in% c("fg3m", "fg2m", "ftm")),
-           aes(fill = "Shots made"),
-           stat = "identity",
-           position = "dodge") +
-  facet_wrap(~namePlayer) +
-  theme_minimal() +
-  scale_fill_manual(values = c("Shots made" = "darkolivegreen3", 
-                               "Shots taken" = "gray")) +
-  labs(x = "",
-       y = "Shots",
-       fill = "")
+both_team_shooting_barplot <- function() {
 
-d %>%
-  filter(dateGame == "2021-10-30" &
-           nameTeam == "Utah Jazz") %>%
-  top_n(n = 3, wt = pts) %>%
-  pivot_longer(cols = c("fga", "fgm", "fta", "ftm", "fg2a", "fg2m", "fg3m", "fg3a"), 
-               names_to = "made_miss",
-               values_to = "value") %>%
-  mutate(shot_type = case_when(
-    made_miss %in% c("fga", "fgm") ~ "All FGs",
-    made_miss %in% c("fg2a", "fg2m") ~ "2-pt FGs",
-    made_miss %in% c("fg3a", "fg3m") ~ "3-pt FGs",
-    made_miss %in% c("fta", "ftm") ~ "FTs"
-  )) %>%
-  ggplot(aes(x = shot_type, y = value)) +
-  geom_bar(data = . %>% filter(made_miss %in% c("fg3a", "fg2a", "fta")),
-           aes(fill = "Shots taken"),
-           color = "darkolivegreen3",
-           alpha = 0.5,
-           stat = "identity",
-           position = "dodge") +
-  geom_bar(data = . %>% filter(made_miss %in% c("fg3m", "fg2m", "ftm")),
-           aes(fill = "Shots made"),
-           stat = "identity",
-           position = "dodge") +
-  facet_wrap("namePlayer") +
-  theme_minimal() +
-  scale_fill_manual(values = c("Shots made" = "darkolivegreen3", 
-                               "Shots taken" = "gray")) +
-  labs(x = "",
-       y = "Shots",
-       fill = "")
+  d %>%
+    filter(dateGame == "2021-10-30" &
+           (slugTeam == "UTA" |
+           slugOpponent == "UTA")) %>%
+    arrange(slugTeam) %>%
+    group_by(slugTeam) %>%
+    top_n(n = 3, wt = pts) %>%
+    ungroup() %>%
+    pivot_longer(cols = c("fga", "fgm", "fta", "ftm", "fg2a", "fg2m", "fg3m", "fg3a"), 
+                 names_to = "made_miss",
+                 values_to = "value") %>%
+    mutate(shot_type = case_when(
+      made_miss %in% c("fga", "fgm") ~ "All FGs",
+      made_miss %in% c("fg2a", "fg2m") ~ "2-pt FGs",
+      made_miss %in% c("fg3a", "fg3m") ~ "3-pt FGs",
+      made_miss %in% c("fta", "ftm") ~ "FTs"
+    )) %>%
+    ggplot(aes(x = shot_type, y = value)) +
+    geom_bar(data = . %>% filter(made_miss %in% c("fg3a", "fg2a", "fta")),
+             aes(fill = "Shots taken"),
+             color = "darkolivegreen3",
+             alpha = 0.5,
+             stat = "identity",
+             position = "dodge") +
+    geom_bar(data = . %>% filter(made_miss %in% c("fg3m", "fg2m", "ftm")),
+             aes(fill = "Shots made"),
+             stat = "identity",
+             position = "dodge") +
+    facet_wrap(namePlayer ~ pts) +
+    theme_minimal() +
+    scale_fill_manual(values = c("Shots made" = "darkolivegreen3", 
+                                 "Shots taken" = "gray")) +
+    labs(x = "",
+         y = "Shots",
+         fill = "")
+  
+}
 
+team_shooting_barplot <- function(date, team, n) {
+
+  d %>%
+    filter(dateGame == date &
+             nameTeam == team) %>%
+    top_n(n = n, wt = pts) %>%
+    pivot_longer(cols = c("fga", "fgm", "fta", "ftm", "fg2a", "fg2m", "fg3m", "fg3a"), 
+                 names_to = "made_miss",
+                 values_to = "value") %>%
+    mutate(shot_type = case_when(
+      made_miss %in% c("fga", "fgm") ~ "All FGs",
+      made_miss %in% c("fg2a", "fg2m") ~ "2-pt FGs",
+      made_miss %in% c("fg3a", "fg3m") ~ "3-pt FGs",
+      made_miss %in% c("fta", "ftm") ~ "FTs"
+    )) %>%
+    ggplot(aes(x = shot_type, y = value)) +
+    geom_bar(data = . %>% filter(made_miss %in% c("fg3a", "fg2a", "fta")),
+             aes(fill = "Shots taken"),
+             color = "darkolivegreen3",
+             alpha = 0.5,
+             stat = "identity",
+             position = "dodge") +
+    geom_bar(data = . %>% filter(made_miss %in% c("fg3m", "fg2m", "ftm")),
+             aes(fill = "Shots made"),
+             stat = "identity",
+             position = "dodge") +
+    facet_grid(~namePlayer) +
+    theme_minimal() +
+    scale_fill_manual(values = c("Shots made" = "darkolivegreen3", 
+                                 "Shots taken" = "gray")) +
+    labs(x = "",
+         y = "Shots",
+         fill = "")
+}
+
+team_shooting_barplot("2021-10-30", "Utah Jazz", 2)
