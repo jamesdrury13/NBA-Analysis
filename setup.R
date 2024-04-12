0b08e034cad24eede14ae09b0fb65b7bdb85cab3
jamesdrury13 <107425976+jamesdrury13@users.noreply.github.com>
2024-04-12 04:34:55 -0700

    Add files via upload

---
+++ setup.R
@@ -0,0 +1,36 @@
+Sys.setenv("VROOM_CONNECTION_SIZE" = 256000)
+
+library(remotes)
+# install_github('jyablonski/practice_package')
+# install_github("d-edison/theoddsapi")
+devtools::install_github("abresler/nbastatR")
+library("nbastatR")
+library(theoddsapi)
+
+library(nbastatR)
+library(tidyverse)
+library(ggthemes)
+library(jsonlite)
+library(httr)
+library(rvest)
+library(scales)
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(shinyjs)
+library(writexl)
+# library(jd.nba)
+
+rm(list = ls())
+
+assign_nba_players()
+assign_nba_teams()
+useShinyjs()
+
+path = "C:/Users/jdrury/OneDrive - Mathematica/Documents/3_Projects/NBA Analysis/"
+setwd(path)
+
+
+
+
+
